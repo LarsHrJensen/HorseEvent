863b1671d1f05aa0e5c8c9cd5cc030290cfd2532
Lars Jensen <laje63255@edu.ucl.dk>
2025-08-21 14:21:20 +0200

    Add project files.

---
+++ HorseEvent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HorseEvent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
