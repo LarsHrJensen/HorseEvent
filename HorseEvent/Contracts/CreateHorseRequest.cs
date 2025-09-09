170bc6fd0649bd7ef4ce18ca59e6d22364e95988
Alfie134 <alberteoegendahl@gmail.com>
2025-09-09 14:10:55 +0200

    variabler til request og response til create horse added

--- HorseEvent/Contracts/CreateHorseRequest.cs
+++ HorseEvent/Contracts/CreateHorseRequest.cs
@@ -3,7 +3,7 @@ namespace Contracts
     public class CreateHorseRequest
     {
         public string Name { get; set; }
-        public int Id { get; set; }
+        public int HorseId { get; set; }
         public int Height { get; set; }
         public int BirthYear { get; set; }
     }
--- HorseEvent/Contracts/HorseResponse.cs
+++ HorseEvent/Contracts/HorseResponse.cs
@@ -1,9 +1,11 @@
-< Project Sdk = "Microsoft.NET.Sdk" >
+namespace Contracts
+{
+    public class HorseResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HorseId { get; set; }
+        public int BirthYear { get; set; }
 
-  < PropertyGroup >
-    < TargetFramework > net9.0 </ TargetFramework >
-    < ImplicitUsings > enable </ ImplicitUsings >
-    < Nullable > enable </ Nullable >
-  </ PropertyGroup >
-
-</ Project >
+    }
+}
