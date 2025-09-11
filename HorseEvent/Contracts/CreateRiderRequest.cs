a26530aa9a53559f3dbc1590109673ecf2877041
Alfie134 <alberteoegendahl@gmail.com>
2025-09-11 10:57:13 +0200

    riderrequest og riderresponse oprettet med variabler

---
+++ HorseEvent/Contracts/CreateRiderRequest.cs
@@ -0,0 +1,9 @@
+namespace Contracts
+{
+    public class CreateRiderRequest
+    {
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public string Email { get; set; }
+    }
+}
