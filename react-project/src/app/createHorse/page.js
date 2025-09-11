c5b91ca5d7f608a7694598435ad9feb8896e4c40
Alfie134 <alberteoegendahl@gmail.com>
2025-09-11 12:06:46 +0200

    variabler, handleChange, Handlesumbit added

--- react-project/src/app/createHorse/page.js
+++ react-project/src/app/createHorse/page.js
@@ -2,6 +2,46 @@ import { useState } from "react";
 import "./page.css";
 
 export default function CreateHorsePage() {
+    // Data for horses = basis for input fields for create object
+    const [horseData, setHorseData] = useState({
+        name: "",
+        id: "",
+        height: "", 
+        birthyear: ""
+    });
+
+    // Handle changes of input
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setHorseData({...horseData, [name]: value});
+    };
+    
+    
+    const handleSumbit = async (e) => {
+        e.preventDefault();
+
+        try{
+            const response = await fetch("https://localhost:5001/api/horses", { //opdater med rigtig URL
+                method: "POST",
+                headers: {
+                    "Conent-Type": "application/json"
+                },
+                body: JSON.stringify(horseData)
+            });
+
+            if(response.ok) {
+                alert("Hest oprettet!");
+                setHorseData({name: "", id: "", height: "", birthyear: ""})
+            } else {
+                alert("Der opstod en fejl.")
+            }
+        } catch (error) {
+            console.error(error);
+            alert("Something went wrong, womp womp")
+        }
+    };
+    
+    
     return (
         <div className="createHorseConatiner">
             <h1> opret hest </h1>
@@ -11,15 +51,15 @@ export default function CreateHorsePage() {
                 type="text"
                 name="name"
                 placeholder="Horse name"
-                value={HorseData.name}
+                value={horseData.name}
                 onChange={handleChange}
                 required>
                 </input>
                 <input
                 type="text"
-                name="id"
+                name="horseId"
                 placeholder="horse id"
-                value={HorseData.horseId}
+                value={horseData.horseId}
                 onChange={handleChange}
                 required>
                 </input>
@@ -27,7 +67,7 @@ export default function CreateHorsePage() {
                 type="number"
                 name="height"
                 placeholder="height"
-                value={HorseData.height}
+                value={horseData.height}
                 onChange={handleChange}
                 required>
                 </input>
@@ -35,11 +75,12 @@ export default function CreateHorsePage() {
                 type="number"
                 name="birthyear"
                 placeholder="birthyear"
-                value={HorseData.birthyear}
+                value={horseData.birthyear}
                 onChange={handleChange}
                 required>
                 </input>
+                <button type="sumbit">Opret Hest</button>
             </form>
         </div>
-    )
+    );
 }
