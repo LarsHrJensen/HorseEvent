7b368cd70824b35df2469f762162715c5baaac9d
Lars Jensen <laje63255@edu.ucl.dk>
2025-09-16 14:05:39 +0200

    Horse og Rider rettet

--- HorseEvent/Models/Horse.cs
+++ HorseEvent/Models/Horse.cs
@@ -2,7 +2,8 @@
 {
     public class Horse
     {
-        public string HorseId { get; set; }
+        public int HorseId { get; set; }
+        public string HoreseUELN { get; set; }
         public string? HorseName { get; set; }
         public decimal Height { get; set; }
         public int BirthYear { get; set; }
--- HorseEvent/Models/Rider.cs
+++ HorseEvent/Models/Rider.cs
@@ -2,7 +2,7 @@
 {
     public class Rider
     {
-        public string RiderId { get; set; }
+        public int RiderId { get; set; }
         public string? RiderName { get; set; }
         public DateTime? BirthDate { get; set; }
         public bool MembershipStatus { get; set; }
