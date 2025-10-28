ea0cdfcd8c78cc7a080cf27834ef9f4d142e9b9d
alfie134 <alberteoegendahl@gmail.com>
2025-10-28 15:38:23 +0000

    første sæt knapper til home page

--- react-project/src/app/home/page.js
+++ react-project/src/app/home/page.js
@@ -4,9 +4,14 @@ import "./page.css"
 export default function HomePage() {
     return(
         <main className="flex flex-col items-center justify-center min-h-screen space-y-6">
-            <h1 className="text-4xl font-bold mb-4 text-center text-top text-[#243C80]"> 
-                Welcome back, User! 
-            </h1>
+            <div className="flex space-x-6">
+                <h1 className="text-4xl font-bold mb-4 text-center text-top text-[#243C80]"> 
+                    Welcome back, User! 
+                </h1>
+                <button type="button" className="btn-default"> Stævner </button>
+                <button type="buton" className="btn-default btn-default--p"> Hjem </button>
+            </div>
+            
             <div className="flex space-x-6">
                 <div className="box-wrapper box-wrapper--small">
                     <h1 className="text-2xl font-bold mb-4 text-center text-top text-[#1B2D5E]">Competition overview</h1>
@@ -16,14 +21,22 @@ export default function HomePage() {
                     <p className="text-[#1B2D5E]">Competition Status</p>
                 </div>
 
-                <div className="box-wrapper box-wrapper--small">
+                <div className="box-wrapper box-wrapper--small space-y-1">
                     <p className="text-[#1B2D5E]">Quick Actions</p>
+                    <button className="btn-default">Maayybee</button>
+                    <button className="btn-default">Maayybee</button>
+                    <button className="btn-default">Maayybee</button>
+                    <button className="btn-default btn-default--p">Maayybee</button>
                 </div>
             </div>
 
-            <div className="">
-                <div className="box-wrapper box-wrapper--info">
+            <div>
+                <div className="box-wrapper box-wrapper--info flex justify-between items-center w-full">
                     <p>competition progess</p>
+                    <div className="flex gap-x-4">
+                        <button className="btn-default btn-default--w text-[#8e44ad]"> Se Live Opdateringer</button>
+                        <button className="btn-default btn-default--w text-[#8e44ad]"> Download Tidsplan</button>
+                    </div>
                 </div>
             </div>
 
@@ -36,8 +49,6 @@ export default function HomePage() {
                     <p className="text-[#1B2D5E]">schedule</p>
                 </div>
             </div>
-
-            
 
             <div className="flex space-x-6">
                 <div className="box-wrapper">
