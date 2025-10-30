d4ada567527eb8194026ec5b1684b95ff4a34915
alfie134 <alberteoegendahl@gmail.com>
2025-10-30 09:27:08 +0000

    mini commit

--- react-project/src/app/my-horses/page.js
+++ react-project/src/app/my-horses/page.js
@@ -2,5 +2,9 @@
 import "./page.css"
 
 export default function MyHorsesPage() {
-
+    return (
+        <main>
+            <h1 className="text-3xl font-bold text-[#5d80f1]"> Mine Heste </h1>
+        </main>
+    )
 }
